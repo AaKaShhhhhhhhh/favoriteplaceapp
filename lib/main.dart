82c473d5484ba12d35c4113a9429384a4eae95ed
AaKaShhhhhhhhh <aakashkamal47@gmail.com>
2025-01-11 19:14:08 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:favoriteplaceapp/UIscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -25,7 +26,9 @@ final theme = ThemeData().copyWith(
   ),
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     const ProviderScope( child: MyApp())  ,
   );
