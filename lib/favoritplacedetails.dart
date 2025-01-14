a40ca91d5ecf3073a2fbc15fededa79d8cbe0cba
AaKaShhhhhhhhh <aakashkamal47@gmail.com>
2025-01-15 01:07:41 +0530

    .

--- lib/favoritplacedetails.dart
+++ lib/favoritplacedetails.dart
@@ -1,5 +1,15 @@
 import 'dart:io';
 
+
+
+class placelocation{
+
+  const placelocation({required this.address , required this.latitude , required this.longitude});
+  
+  final double longitude;
+  final double latitude;
+  final String address;
+}
 class details{
   details({
   required this.name , required this.image});
